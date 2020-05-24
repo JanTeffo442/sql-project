d34c4291637920c5aeb445219c35498a3c58867c
JanTeffo442 <jan.teffo@umuzi.org>
2020-05-24 23:16:20 +0200

    implementing reviews

---
+++ UmuziDB.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+connection = psycopg2.connect(user = 'user', password = 'pass', 
+	host = '127.0.0.1', port = 5432, database = 'Umuzi')
+
+cursor = connection.cursor()
+
+create_table_query = '''CREATE TABLE Customers(CustomerID INT PRIMARY KEY NOT NULL,
+FirstName VARCHAR(50), LastName VARCHAR(50), Gender VARCHAR,
+Address VARCHAR(200), Phone VARCHAR(15), Email VARCHAR(100), City VARCHAR(20),
+Country VARCHAR(50));'''
+
+cursor.execute(create_table_query)
+connection.commit()
+connection.close()
