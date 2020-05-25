d9fe0cd24d9c2e2f66fc250600ec249a39960f76
JanTeffo442 <jan.teffo@umuzi.org>
2020-05-25 20:13:35 +0200

    SQL queries in python script

--- UmuziDB.py
+++ UmuziDB.py
@@ -21,8 +21,8 @@ Thando','Sithole','Female','240 Sect 1','0794445584','thando@gmail.com','Cape To
 create_table_employees = '''CREATE TABLE Employees(EmployeeID SERIAL PRIMARY KEY,
 FirstName VARCHAR(50), LastName VARCHAR(50), Email VARCHAR(100), JobTitle VARCHAR(20));'''
 
-insert_query_employees = '''INSERT INTO Employees(FirstName, LastName, Email, JobTitle) VALUES('
-Kani','Matthew','mat@gmail.com','Manager'),
+insert_query_employees = '''INSERT INTO Employees(FirstName, LastName, Email, JobTitle) 
+VALUES('Kani','Matthew','mat@gmail.com','Manager'),
 ('Lesly','Cronje','LesC@gmail.com','Clerk'),
 ('Gideon','Maduku','m@gmail.com','Accountant');'''
 
@@ -58,6 +58,74 @@ VALUES('Harley Davidson Chopper', 'This replica features working kickstand, fron
 ('Classic Car', 'Turnable front wheels, steering function', '550.75'),
 ('Sports car', 'Turnable front wheels, steering function', '700.60');'''
 
-cursor.execute(insert_query_orders)
+
+cursor.execute()
 connection.commit()
 connection.close()
+
+
+# Part 2: Querying a database
+# All records fro table Customers
+records = '''SELECT * FROM Customers;'''
+
+#2 records from name column inthe Customer table
+records = '''SELECT FirstName FROM Customers;'''
+
+#3 Name of the Customer whose CustomerID is 1
+name = '''SELECT FirstName FROM Customers WHERE CustomerID = 1;'''
+
+#4 UPDATE the record for CustomerID = 1 on the Customer table so that the name is “Lerato Mabitso”
+update_record = '''UPDATE Customers SET FirstName='Lerato',LastName='Mabitso' WHERE CustomerID=1;'''
+
+#5 DELETE the record from the Customers table for customer 2 (CustomerID = 2).
+ delete_record = '''DELETE FROM Customers WHERE CustomerID=2;'''
+
+#6 Select all unique statuses from the Orders table and get a count of the number of orders for each unique status
+unique_statuses = '''SELECT DISTINCT Status, COUNT(DISTINCT Status) Orders GROUP BY Status;'''
+
+#7 MAXIMUM payment made on the PAYMENTS table
+maximum_payment = '''SELECT MAX(Amount) FROM Payments;'''
+
+#8 all customers from the “Customers” table, sorted by the “Country” column
+sort_by_country = '''SELECT * FROM Customers ORDER BY Country ASC;'''
+
+#9 all products with a price BETWEEN R100 and R600
+price_between = '''SELECT * FROM Products WHERE BuyPrice BETWEEN 100 AND 600;'''
+
+#10 all fields from “Customers” where country is “Germany” AND city is “Berlin”
+Germany_Berlin = '''SELECT * FROM Customers WHERE Country='Germany' and City='Berlin';'''
+
+#11 all fields from “Customers” where city is “Cape Town” OR “Durban”
+CapeTown_or_Durban = '''SELECT * FROM Customers WHERE City='Cape Town' OR 'Durban';'''
+
+#12 all records from Products where the Price is GREATER than R500
+price_greater = '''SELECT * FROM Products WHERE Buyprice > 500;'''
+
+#13 return the sum of the Amounts on the Payments table.
+sum_amount = '''SELECT SUM(Amount) FROM Payments;'''
+
+#14 number of shipped orders in the Orders table
+shipped_orders = '''SELECT COUNT(Status) FROM Orders WHERE Status='Shipped';'''
+
+#15 average price of all Products, in Rands and in Dollars (assume the exchange rate is R12 to the Dollar)
+average_price = '''SELECT AVG(BuyPrice) AS RAND, AVG(BuyPrice/12) AS DOLLARS FROM Products;'''
+
+#16 INNER JOIN create a query that selects all Payments with Customer information.
+join_payments_customer = '''SELECT 
+								Customers.CustomerID,
+								Customers.FirstName,
+								Customers.Lastname,
+								Customers.Gender,
+								Customers.Address,
+								Customers.Phone,
+								Customers.Email,
+								Customers.City,
+								Customers.Country,
+								Payments.PaymentDate,
+								Payments.Amount
+							FROM
+								Customers
+							INNER JOIN Payments ON Customers.CustomerID=Payments.PaymentID;'''
+
+#17 all products that have turnable front wheels
+turnable_wheels = '''SELECT * FROM Products WHERE Description LIKE 'Turnable front wheels%';'''
